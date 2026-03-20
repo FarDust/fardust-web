551ff929f4231edeff29f56119cd3e01c814f2a4
FarDust <gabriel.faundez@gmail.com>
2026-03-20 00:48:55 -0300

    fix(review): address PR 359 comments
    Remove duplicate HttpClient providers from feature modules and fix the Open Source UC community copy raised in review feedback.

--- src/app/common/console/console.module.ts
+++ src/app/common/console/console.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsoleComponent } from './console.component';
 import { CountryService } from '../../services/country.service';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [ConsoleComponent],
   exports: [ConsoleComponent],
   imports: [CommonModule, FontAwesomeModule],
-  providers: [CountryService, provideHttpClient(withInterceptorsFromDi())],
+  providers: [CountryService],
 })
 export class ConsoleModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsoleModule } from '../common/console/console.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
@@ -23,6 +19,5 @@ import { HomeRoutingModule } from './home-routing.module';
     TranslateModule,
     FormsModule,
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class HomeModule {}
